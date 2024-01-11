c91bbdae85a54e110724f55e325f92f4c078d745
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-01-11 20:21:32 +0900

    Initial commit

---
+++ src/main/java/data/Information.java
@@ -0,0 +1,13 @@
+package data;
+
+import java.util.List;
+
+public enum Information {
+    Student_Name,
+    Student_idNumber,
+   EssentialSubject,
+    OptionalSubject,
+
+    Subject_Java,
+   Subject_Spring;
+}
