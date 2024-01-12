c2f53cd917a2c2536497172ee2c4bf16b72f5083
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-01-12 14:37:37 +0900

    수강생 등록 및 조회 클래스 분리하기
    수강생 등록 기능과  수강생 조회 기능을 클래스로 분리 하여, 메인 클래스에서 각 클래스를 호출하는 것으로 수정

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,6 +1,8 @@
 package Main;
 
+import Service.SaveStudentInform;
 import Service.ScreenService;
+import Service.SearchStudentInform;
 import domain.Student;
 import domain.Subject;
 
@@ -14,89 +16,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ScreenService screenService = new ScreenService();
-//        screenService.mainPage();
-//        String input = sc.nextLine();
-//while(true){
-//        switch(input){
-//            case("1"): 해당 메서드 호출
-//                break;
-//            case("2"):
-//                break;
-//            case("3"):
-//                break;
-//            case("4"):
-//                break;
-//            case("5"):
-//                break;
-//            default:
-//
-//                break;
-//        }
-//    }
+        screenService.mainPage();
+        String input = sc.nextLine();
 
-        // 1.수강생 정보 입력하기
-        screenService.inputStudentIdNumber();
-        String studentId = sc.nextLine();
-        screenService.inputStudentName();
-        String studentName = sc.nextLine();
+        Student student = new Student("", "");
+        SaveStudentInform saveStudentInform = new SaveStudentInform();
+        SearchStudentInform searchStudentInform = new SearchStudentInform();
 
-        Student student = new Student(studentId, studentName);
-        student.addToStudentCard();
-
-        // 2.수강생 과목 입력하기
-        Subject subjectE = new Subject(studentId, "");
-
-        // 필수 과목 선택  (3개 미만까지는 계속 while문)
-        while (true) {
-            screenService.selectSubjectE();
-            screenService.essentialSubject();
-            String subjectName = sc.nextLine();
-            subjectE.makeEssentialSubjectList(studentId, subjectName);
-
-            List<String> selectedEssentialSubjects = subjectE.getEessentialSubjectList(studentId);
-            if (selectedEssentialSubjects.size() >= 3) {
-                System.out.println("1. 계속 선택   2. 그만 선택");
-                String keepOrStop = sc.nextLine();
-                if (keepOrStop.equals("2")) {
-                    break;
-                }
-            }
-        }
-
-        // 선택과목 선택
-        Subject subjectO = new Subject(studentId, "");
-
-        while (true) {
-            screenService.selectSubjectO();
-            screenService.optionalSubject();
-            String subjectName = sc.nextLine();
-            subjectO.makeOptionalSubjectList(studentId, subjectName);
-
-            List<String> selectedOptionalSubjects = subjectO.getOptionalSubjectList(studentId);
-            if (selectedOptionalSubjects.size() >= 2) {
-                System.out.println("1. 계속 선택하시겠습니가?   2. 그만 선택 하시겠습니까?  (번호만 입력해 주세요)");
-                String keepOrStop = sc.nextLine();
-                if (keepOrStop.equals("2")) {
-                    break;
-                }
-            }
-        }
-
-
-        // 3.수강생 정보 조회하기
-        screenService.findStudentIdNumber();
-        String searchStudentId = sc.nextLine();
-        screenService.findStudentName();
-        String searchStudentName = sc.nextLine();
-
-        if (searchStudentId.equals(studentId) && searchStudentName.equals(studentName)) {
-            System.out.println("수강생 아이디 : " + student.getStudentId());
-            System.out.println("수강생 이름 : " + student.getStudentName());
-            System.out.println("수강한 과목:");
-
-
+while(true){
+        switch(input){
+            case("1"):  saveStudentInform.saveStudentInform();
+                break;
+            case("2"):  searchStudentInform.searchStudentInform(student.getStudentId(), student.getStudentName());
+                break;
+            case("3"):
+                break;
+            case("4"):
+                break;
+            case("5"):
+                break;
+            default:
+                break;
         }
     }
 
 
+
+
+
+    }
 }
--- src/main/java/data/ScreenData.java
+++ src/main/java/data/ScreenData.java
@@ -35,6 +35,6 @@ public enum ScreenData {
     subjectTypeB,
 
     MainPage,
-    SelectSubject;
-
+    SelectSubject,
+    AskKeepOrStop;
 }
