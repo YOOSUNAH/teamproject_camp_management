85f791676cd8544f009629a319fd5fc90af55221
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-01-12 15:54:07 +0900

    브랜치 수정으로 인한 커밋

--- src/main/java/data/ScreenData.java
+++ src/main/java/data/ScreenData.java
@@ -1,8 +1,7 @@
 package data;
 
 public enum ScreenData {
-    Student_Name,
-    Student_IdNumber,
+
     Input_Student_IdNumber,
     Input_Student_Name,
     Find_Student_Name,
@@ -15,21 +14,13 @@ public enum ScreenData {
    EssentialSubject_3,
    EssentialSubject_4,
    EssentialSubject_5,
-    Java,
-    객체지향,
-    Spring,
-    JPA,
-    MySQL,
 
     OptionalSubject,
     OptionalSubject_1,
     OptionalSubject_2,
     OptionalSubject_3,
     OptionalSubject_4,
-    디자인_패턴,
-    Spring_Security,
-    Redis,
-    MongoDB,
+
 
     subjectTypeA,
     subjectTypeB,
