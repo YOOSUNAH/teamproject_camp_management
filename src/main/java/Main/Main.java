03e532e43803b5292238970a5943a282839f760e
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-01-15 16:49:27 +0900

    인영님, 영효님 작성한 코드 적용 (컴파일 오류만 안나게 수정)

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         ScreenService screenService = new ScreenService();
         StudentAppender studentAppender = new StudentAppender();
@@ -16,29 +15,27 @@ public class Main {
         StudentDeleter studentDeleter = new StudentDeleter();
         CalculateService calculateService = new CalculateService();
         Score putScore = new Score();
-        while (true) {
-            screenService.mainPage();
-            String input = sc.nextLine();
-            switch (input) {
-                case "1" -> {
-                    studentAppender.append();
-                    putScore.setInitialScoreData(String.valueOf(studentAppender.getIDforscore()));
-                }
-                case "2" -> {
-                    studentReader.read();
-                }
-                case "3" -> {
-                    putScore.RecordScore();
-                }
-                case "4" -> {
-                    studentDeleter.delete();
-                }
-                case "5" -> {
-                    calculateService.getGradeBySubjectAndTimes();
+            while (true) {
+                screenService.mainPage();
+                String input = sc.nextLine();
+                switch (input) {
+                    case "1" -> {
+                        studentAppender.append();
+                        putScore.setInitialScoreData(String.valueOf(studentAppender.getIDforscore()));
+                    }
+                    case "2" -> {
+                        studentReader.read();
+                    }
+                    case "3" -> {
+                        putScore.RecordScore();
+                    }
+                    case "4" -> {
+                        studentDeleter.delete();
+                    }
+                    case "5" -> {
+                        calculateService.getGradeBySubjectAndTimes();
+                    }
                 }
             }
         }
-
-
     }
-}
